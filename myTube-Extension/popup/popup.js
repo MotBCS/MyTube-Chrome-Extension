c2e8bda26dcc07a0ecc48583a5f0178a77b7b0c1
Mya Thomas <82474687+MotBCS@users.noreply.github.com>
2023-12-05 16:18:10 -0500

    Delete myTube-Extension directory

--- myTube-Extension/popup/popup.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.sync.get(['block_comments', 'left-side-bar', 'recommendations', 'home_feed', 'shorts'], function(obj) {
-    document.getElementById('comments-opt').checked = obj.block_comments == undefined ? true : obj.block_comments;
-
-
-});
-
-document.addEventListener('DOMContentLoaded', function(){
-    
-});
